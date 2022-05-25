0b1544101be5ec469243cec0151acbd93b2edaf5
huycq1712 <huysk82000@gmail.com>
2022-05-26 00:34:42 +0700

    implement collate_fn

--- data/collate_batch.py
+++ data/collate_batch.py
@@ -1,5 +1,11 @@
-# encoding: utf-8
-"""
-@author:  sherlock
-@contact: sherlockliao01@gmail.com
-"""
+from cProfile import label
+import torch
+
+def collate_fn(batch):
+    batch = list(zip(*batch))
+    images = torch.stack(batch[0])
+    labels = batch[1]
+    labels_1 = batch[2]
+    labels_5 = batch[3]
+    
+    return images, labels, labels_1, labels_5
