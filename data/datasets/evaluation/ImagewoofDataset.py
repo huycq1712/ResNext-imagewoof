7efdaa1fbdff74f2d008ccc1556a08c88a3112e5
huycq3110 <huysk82000@gmail.com>
2022-05-27 17:00:20 +0700

    fix pep8

---
+++ data/datasets/evaluation/ImagewoofDataset.py
@@ -0,0 +1,15 @@
+import torch
+import logging
+import numpy as np
+
+
+def accuracy(predicts, labels):
+    pass
+
+
+def f1_score(predicts, labels):
+    pass
+
+
+def auc(predicts, labels):
+    pass
